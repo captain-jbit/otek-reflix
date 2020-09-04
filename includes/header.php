8de40aea2221ea664b02821c76a2dd07bed39c5b
captain-jbit <70452176+captain-jbit@users.noreply.github.com>
2020-09-05 01:00:31 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,10 @@ require_once("includes/classes/PriviewProvider.php");
 require_once("includes/classes/CategoryContainers.php");
 require_once("includes/classes/Entity.php");
 require_once("includes/classes/EntityProvider.php");
+require_once("includes/classes/ErrorMessage.php");
+require_once("includes/classes/SeasonProvider.php");
+require_once("includes/classes/Season.php");
+require_once("includes/classes/Video.php");
 
 #check if user logged in by session
 if(!isset($_SESSION["userLoggedIn"])){
@@ -24,7 +28,7 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
     <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
 
     <script src="https://kit.fontawesome.com/f0b5a27c73.js" crossorigin="anonymous"></script>
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="assets/js/script.js"></script>
 </head>
