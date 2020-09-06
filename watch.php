df81aa63e7250300c85e599c90bb6e36b755e15e
captain-jbit <70452176+captain-jbit@users.noreply.github.com>
2020-09-07 00:35:44 +0300

    Update watch.php

--- watch.php
+++ watch.php
@@ -9,15 +9,26 @@ if(!isset($_SESSION["userLoggedIn"])){
 }
 
 $video = new Video($con,$_GET["id"]);
-$videos->incrementViews();
+$video->incrementViews();
+
 
 ?>
 
 <div class="watchContainer">
 
+<div class="videoControls watchNav"> 
+    <button onclick="goBack()"><i class="fas fa-arrow-left"></i></button>
+    <h1><?php echo $video->getTitle();?></h1>
+</div>
+
+<div class="videoControls upNext">
+<button><i class="fas fa-redo"></button>
+</div>
 
 <video conttrols autoplay>
     <source src='<?php echo $video->getFilePath();?>' type="video/mp4"/>
 </video>
+
 </div>
 
+<script>initVideo("<?php echo $video->getId(); ?>","<?php echo $userLoggedIn?>");</script>
